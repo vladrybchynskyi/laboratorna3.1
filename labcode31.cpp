35de6de65c37af0f98c90208d88ce374eab2f9e7
vladrybchynskyi <vladyslav.rybchynskyi.ri.2024@lpnu.ua>
2024-09-19 09:12:12 +0300

    Update labcode31.cpp

--- labcode31.cpp
+++ labcode31.cpp
@@ -1,13 +1,18 @@
+// labcode31.cpp
+// Рибчинський Владислав
+// Лабораторна робота №3.1
+// Варіант 12
 #include <iostream>
 #include <cmath>
 using namespace std;
 int main() {
- double x;
- double y;
- double A;
- double B;
+ double x; // вхідний параметр
+ double y; // результат обчислення
+ double A; // проміжний результат 
+ double B; // проміжний результат
  cout << "x= ", cin >> x;
  A = ((2+x)/x*x) + 1;
+   //Розгалуження в скороченій формі
    if (x < 0)
    B = x *x - 2 *x *x *x *x;
    if (x >= 0 && 2 >= x) 
@@ -17,6 +22,7 @@ int main() {
    y = A + B;
    cout << endl;
    cout << "1) y =" << y << endl;
+   // Повне розгалуження
    if (x < 0)
    B = x *x *x -2;
      else
